ce60c91b48401d867b89388255491b4d3b0cdf25
thinh2313 <73292324+thinh2313@users.noreply.github.com>
2021-11-04 15:34:07 +0700

    update js

--- LaptopShop/User/wwwroot/js/site.js
+++ LaptopShop/User/wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var sitePlusMinus = function () {
+	$('.js-btn-minus').on('click', function (e) {
+		e.preventDefault();
+		if ($(this).closest('.input-group').find('.form-control').val() != 0) {
+			$(this).closest('.input-group').find('.form-control').val(parseInt($(this).closest('.input-group').find('.form-control').val()) - 1);
+		} else {
+			$(this).closest('.input-group').find('.form-control').val(parseInt(0));
+		}
+	});
+	$('.js-btn-plus').on('click', function (e) {
+		e.preventDefault();
+		$(this).closest('.input-group').find('.form-control').val(parseInt($(this).closest('.input-group').find('.form-control').val()) + 1);
+	});
+};
+sitePlusMinus();
